79c9ae30698b9caae3f8e1695115abd539fbe999
Alex So <alex.so@lidl.com.hk>
2024-07-14 10:42:46 +0800

    first trial on hsbc valuation using puppeteer

---
+++ constant/constants.js
@@ -0,0 +1,5 @@
+export const HSBC_VALUATION_URL = 'https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/';
+export const SCREENSHOT_WIDTH = 1080;
+export const SCREENSHOT_HEIGHT = 1024;
+export const SCREENSHOT_SCROLL_X = 0;
+export const SCREENSHOT_SCROLL_Y = 450;
