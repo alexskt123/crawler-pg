8eedc8d7b6cc55b44e4b8604866c84c4524b9c3a
Alex So <alex.so@lidl.com.hk>
2024-07-14 10:51:42 +0800

    add commander for different options and google search example

--- constant/constants.js
+++ constant/constants.js
@@ -3,3 +3,5 @@ export const SCREENSHOT_WIDTH = 1080;
 export const SCREENSHOT_HEIGHT = 1024;
 export const SCREENSHOT_SCROLL_X = 0;
 export const SCREENSHOT_SCROLL_Y = 450;
+
+export const GOOGLE_SEARCH_URL = 'https://www.google.com.hk/';
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { captureValuationImage } from './lib/hsbcValuation.js';
+import { captureSearchedImage } from './lib/googleSearch.js';
+import { program } from 'commander';
 
-captureValuationImage();
+program
+    .option('--hsbc')
+    .option('--google');
+
+program.parse();
+
+const { hsbc, google } = program.opts();
+
+hsbc && captureValuationImage();
+google && captureSearchedImage();
